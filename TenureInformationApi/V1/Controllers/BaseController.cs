63633c9e8a20f91d1f6474a1f14aa6908253be2b
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-05-28 10:04:54 +0100

    Feature/mttl 691 dynamodb healthcheck (#9)
    * DynamoDb healthcheck. Refactor to fully use Hackney Core package
    
    * Minor change

--- TenureInformationApi/V1/Controllers/BaseController.cs
+++ TenureInformationApi/V1/Controllers/BaseController.cs
@@ -1,10 +1,10 @@
-using System.Collections.Generic;
-using System.Linq;
-using TenureInformationApi.V1.Infrastructure;
+using Hackney.Core.Middleware;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TenureInformationApi.V1.Controllers
 {
@@ -18,7 +18,7 @@ namespace TenureInformationApi.V1.Controllers
         public string GetCorrelationId()
         {
             StringValues correlationId;
-            HttpContext.Request.Headers.TryGetValue(Constants.CorrelationId, out correlationId);
+            HttpContext.Request.Headers.TryGetValue(HeaderConstants.CorrelationId, out correlationId);
 
             if (!correlationId.Any())
                 throw new KeyNotFoundException("Request is missing a correlationId");
