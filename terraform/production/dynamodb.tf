1ab5ca00537575e2af0160913215df9ef87bbe82
Charnjit Sohal <charnjit.sohal@news.co.uk>
2021-09-02 16:14:05 +0100

    added TF code for staging and production

--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -1,24 +1,21 @@
 resource "aws_dynamodb_table" "tenureinformationapi_dynamodb_table" {
-    name                  = "TenureInformation"
-    billing_mode          = "PROVISIONED"
-    read_capacity         = 10
-    write_capacity        = 10
-    hash_key              = "id"
+  name           = "TenureInformation"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "id"
 
-    attribute {
-        name              = "id"
-        type              = "S"
-    }
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
-    tags = {
-        Name              = "tenure-information-api-${var.environment_name}"
-        Environment       = var.environment_name
-        terraform-managed = true
-        project_name      = var.project_name
-        BackupPolicy     = "Prod"
-    }
-    
-    point_in_time_recovery {
-        enabled           = true
-    }
+  tags = merge(
+    local.default_tags,
+    { BackupPolicy = "Prod" }
+  )
+
+  point_in_time_recovery {
+    enabled = true
+  }
 }
