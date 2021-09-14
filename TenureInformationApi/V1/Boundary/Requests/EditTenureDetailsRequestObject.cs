4a9713f64204648fd156fa21c950d2a0273fa9a8
Callum Macpherson <callum.macphers@hackney.gov.uk>
2021-09-14 16:03:02 +0100

    Update EditTenureDetailsRequestObject - set StartOfTenureDate nullable.

--- TenureInformationApi/V1/Boundary/Requests/EditTenureDetailsRequestObject.cs
+++ TenureInformationApi/V1/Boundary/Requests/EditTenureDetailsRequestObject.cs
@@ -8,7 +8,7 @@ namespace TenureInformationApi.V1.Boundary.Requests
 {
     public class EditTenureDetailsRequestObject
     {
-        public DateTime StartOfTenureDate { get; set; }
+        public DateTime? StartOfTenureDate { get; set; }
         public DateTime? EndOfTenureDate { get; set; }
         public TenureType TenureType { get; set; }
     }
