e67d7b23116438a4854dc68e502ad50451623b8c
mikemoody-amido <77778488+mikemoody-amido@users.noreply.github.com>
2021-09-15 16:46:12 +0100

    Re-enabled other gateway tests

--- TenureInformationApi.Tests/V1/Gateways/DynamoDbGatewayTests.cs
+++ TenureInformationApi.Tests/V1/Gateways/DynamoDbGatewayTests.cs
@@ -118,7 +118,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             _logger.VerifyExact(LogLevel.Debug, $"Calling IDynamoDBContext.LoadAsync for id {request.Id}", Times.Once());
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task PostNewTenureSuccessfulSaves()
         {
             // Arrange
@@ -143,7 +143,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             _cleanup.Add(async () => await _dynamoDb.DeleteAsync(dbEntity).ConfigureAwait(false));
         }
 
-        [Theory(Skip = "Test")]
+        [Theory]
         [InlineData(false)]
         [InlineData(true)]
         public async Task UpdateTenureWithNewHouseHoldMemberSuccessfullyUpdates(bool nullTenuredAssetType)
@@ -191,7 +191,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             result.NewValues["householdMembers"].Should().BeEquivalentTo(result.UpdatedEntity.HouseholdMembers);
         }
 
-        [Theory(Skip = "Test")]
+        [Theory]
         [InlineData(false)]
         [InlineData(true)]
         public async Task UpdateExistingHouseHoldMembersInTenureSuccessfullyUpdates(bool nullTenuredAssetType)
@@ -232,7 +232,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             _cleanup.Add(async () => await _dynamoDb.DeleteAsync<TenureInformationDb>(entity.Id).ConfigureAwait(false));
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenTenureDoesntExistReturnsNull()
         {
             var mockQuery = _fixture.Create<TenureQueryRequest>();
@@ -246,7 +246,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             response.Should().BeNull();
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenNoChangesAreInRequestObjectDatabaseIsntUpdated()
         {
             // create mock tenure
@@ -285,7 +285,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             databaseResponse.TenureType.Code.Should().Be(mockTenure.TenureType.Code);
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenCalledDatabaseIsUpdated()
         {
             // create mock tenure
@@ -325,7 +325,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             databaseResponse.TenureType.Code.Should().Be(updaterResponse.UpdatedEntity.TenureType.Code);
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenStartDateIsInRequestButNoEndDateIsInDatabaseNoExceptionIsThrown()
         {
             // create mock tenure - end date is null
@@ -354,7 +354,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             act.Should().NotThrow<EditTenureInformationValidationException>();
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenStartDateIsInRequestButEndDateInDatabaseIsGreaterNoExceptionIsThrown()
         {
             var tenureStartDate = _fixture.Create<DateTime>();
@@ -387,7 +387,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             act.Should().NotThrow<EditTenureInformationValidationException>();
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenStartDateIsInRequestButEndDateInDatabaseIsLessExceptionIsThrown()
         {
             // start date passed, end date is less - throw error
@@ -420,7 +420,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             act.Should().Throw<EditTenureInformationValidationException>();
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenEndDateIsInRequestButStartDateInDatabaseIsLessExceptionIsNotThrown()
         {
             var tenureEndDate = _fixture.Create<DateTime>();
@@ -452,7 +452,7 @@ namespace TenureInformationApi.Tests.V1.Gateways
             act.Should().NotThrow<EditTenureInformationValidationException>();
         }
 
-        [Fact(Skip = "Test")]
+        [Fact]
         public async Task EditTenureDetailsWhenEndDateIsInRequestButStartDateInDatabaseIsGreaterExceptionIsThrown()
         {
             var tenureEndDate = _fixture.Create<DateTime>();
