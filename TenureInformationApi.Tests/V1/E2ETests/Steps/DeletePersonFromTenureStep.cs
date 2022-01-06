93782ff171e0db1bd80504e32db33bfa9aae8c6b
humulla <43747248+humulla@users.noreply.github.com>
2022-01-06 11:17:26 +0000

    Updated verify sns tests

--- TenureInformationApi.Tests/V1/E2ETests/Steps/DeletePersonFromTenureStep.cs
+++ TenureInformationApi.Tests/V1/E2ETests/Steps/DeletePersonFromTenureStep.cs
@@ -80,7 +80,9 @@ namespace TenureInformationApi.Tests.V1.E2ETests.Steps
             };
 
             var snsVerifer = snsFixture.GetSnsEventVerifier<EntityEventSns>();
-            // (await snsVerifer.VerifySnsEventRaised<EntityEventSns>(verifyFunc)).Should().BeTrue(snsVerifer.LastException?.Message);
+            var snsResult = await snsVerifer.VerifySnsEventRaised(verifyFunc);
+            if (!snsResult && snsVerifer.LastException != null)
+                throw snsVerifer.LastException;
         }
 
         private void VerifyEventData(object eventDataJsonObj, List<HouseholdMembers> expected)
