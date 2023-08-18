4ed25a1a7c73429407ae3245c5c8c92033c3e7f4
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2023-08-18 16:54:41 +0100

    Add new package version and foundation work for new listener event

---
+++ TenureInformationApi/V1/Infrastructure/JWT/UpdateTenureEventConstants.cs
@@ -0,0 +1,11 @@
+namespace TenureInformationApi.V1.Infrastructure
+{
+    public static class UpdateTenureEventConstants
+    {
+        // JWT TOKEN
+        public const string V1_VERSION = "v1";
+        public const string EVENTTYPE = "TenureUpdatedEvent";
+        public const string SOURCE_DOMAIN = "Tenure";
+        public const string SOURCE_SYSTEM = "TenureAPI";
+    }
+}
