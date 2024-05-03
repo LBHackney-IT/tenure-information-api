bbca97c9873538235bf04c3a22ad991f0769742a
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-05-03 18:11:25 +0100

    Changes to continue for the next week

--- TenureInformationApi/build_old.sh
+++ TenureInformationApi/build_old.sh
@@ -1,23 +1,21 @@
-#!/bin/ash
+#!/bin/bash
 
 #install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
-# if [ -f /etc/debian_version ]
-# then
-  apk update -q
-  apk add --no-cache zip
-# fi
-# check zip version maybe it comes by default
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install zip
+fi
 
 #dotnet restore
 dotnet tool install --global Amazon.Lambda.Tools --version 4.0.0
 
 
-# can check that by default, but since we're using a dotnet image, it should already be set
 # (for CI) ensure that the newly-installed tools are on PATH
-# if [ -f /etc/debian_version ]
-# then
-#   export PATH="$PATH:/$(whoami)/.dotnet/tools"
-# fi
+if [ -f /etc/debian_version ]
+then
+  export PATH="$PATH:/$(whoami)/.dotnet/tools"
+fi
 
 dotnet restore
 dotnet lambda package --configuration release --framework net6.0 --output-package ./bin/release/net6.0/tenure-information-api.zip
