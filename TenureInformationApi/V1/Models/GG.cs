83a4dd6589504d15939ad20764fd7834639ae516
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-06-13 17:36:17 +0100

    return type on dummy cocde

--- TenureInformationApi/V1/Models/GG.cs
+++ TenureInformationApi/V1/Models/GG.cs
@@ -3,7 +3,7 @@ namespace TenureInformationApi.V1.Models
 {
     public static class GG
     {
-        public static JustANumber()
+        public static int JustANumber()
         {
             return new FF().GetFirstNumber();
         }
