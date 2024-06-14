63097324b264cceb0701584991137f1c9955050d
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-06-14 12:24:32 +0100

    no tests dummy

---
+++ TenureInformationApi/V1/Models/DD.cs
@@ -0,0 +1,10 @@
+namespace TenureInformationApi.V1.Models
+{
+    public static class DD
+    {
+        public static int JustANumber()
+        {
+            return new FF().GetFirstNumber() * 8;
+        }
+    }
+}
