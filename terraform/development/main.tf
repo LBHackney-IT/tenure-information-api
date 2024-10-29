d9abcd77e26f6a3223647988b456ba52aa4fd2ab
humulla <43747248+humulla@users.noreply.github.com>
2024-10-29 15:37:43 +0000

    add dynamodb stream

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -142,5 +142,12 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 
       sid = "dev-statement"
     }	
-}	
+}
+
+# TO DO: Add lambda function name once created
+resource "aws_lambda_event_source_mapping" "aws_lambda_event_source" {
+  event_source_arn  = aws_dynamodb_table.dynamodb_table.stream_arn
+  function_name     = aws_lambda_function.lambda_function.arn
+  starting_position = "LATEST"
+}
 
--- terraform/production/dynamodb.tf
+++ terraform/production/dynamodb.tf
@@ -1,7 +1,9 @@
 resource "aws_dynamodb_table" "tenureinformationapi_dynamodb_table" {
-  name           = "TenureInformation"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  name             = "TenureInformation"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "id"
+  stream_enabled   = true
+  stream_view_type = "KEYS_ONLY"
 
   attribute {
     name = "id"
