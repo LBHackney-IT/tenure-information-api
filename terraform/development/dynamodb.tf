3e375eb7c8fee21fb6612784dec91cb13eb9eac3
Humairaa Mulla <humairaa.mulla@hackney.gov.uk>
2024-12-03 13:48:43 +0000

    update stream view type

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -3,7 +3,7 @@ resource "aws_dynamodb_table" "tenureinformationapi_dynamodb_table" {
   billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "id"
   stream_enabled   = true
-  stream_view_type = "KEYS_ONLY"
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "id"
