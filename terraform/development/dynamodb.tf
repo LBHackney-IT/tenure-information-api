0874a38e40f601e2fe601e663fd97c21f1688c37
Adam <adam.tracy20@gmail.com>
2025-01-22 10:57:11 +0000

    Fix depends on

--- terraform/development/dynamodb.tf
+++ terraform/development/dynamodb.tf
@@ -42,6 +42,5 @@ resource "aws_lambda_event_source_mapping" "aws_lambda_event_source" {
   event_source_arn  = aws_dynamodb_table.tenureinformationapi_dynamodb_table.stream_arn
   function_name     = data.aws_lambda_function.dynamodb_stream_trigger.arn
   starting_position = "LATEST"
+  depends_on = [aws_dynamodb_table.tenureinformationapi_dynamodb_table]
 }
-
-
